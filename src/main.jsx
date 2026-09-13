e43faeadfb85cad0b423069ae9a345c3cde4000d
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-09-12 22:11:58 -0300

    feat: add login page

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/auth/authProvider.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import './index.module.css'
 import { App } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
